4af5b1f393f821b91c9f779b329719117df8f5b6
Tonie <tonie.lanquette.simplon@gmail.com>
2024-09-19 17:01:53 +0200

    profil ok, working on champion page

--- src/Controller/Front/ChampionFrontController.php
+++ src/Controller/Front/ChampionFrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\ChampionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,10 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class ChampionFrontController extends AbstractController
 {
     #[Route(name: 'app_champion_front_index')]
-    public function index(): Response
+    public function index(ChampionRepository $championRepository): Response
     {
+        $champions = $championRepository->findAll();
+
         return $this->render('front/champion/index.html.twig', [
-            'controller_name' => 'ChampionFrontController',
+            'champions'-> $champions
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,15 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['username'], message: 'This username is already taken')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
