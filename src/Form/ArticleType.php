d9e3db82651bd5ae9648d00cf52b0c3c0cded711
Tonie <tonie.lanquette.simplon@gmail.com>
2024-09-30 01:02:53 +0200

    29/09

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Entity\Build;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,16 +15,29 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('build', EntityType::class, [
+            ->add('build', EntityType::class, [
                 'class' => Build::class,
                 'choice_label' => 'title',
+                'label' => 'Target  :'
             ])
-            ->add('title')
-            ->add('introduction')
-            ->add('starter_explication')
-            ->add('core_explication')
-            ->add('optional_explication')
-            ->add('conclusion');
+            ->add('title', null,[
+                'label' => 'Title :',
+            ])
+            ->add('introduction', null, [
+                'label' => 'Introduction :',
+            ])
+            ->add('starter_explication', null, [
+            'label' => 'About starter items :',
+            ])
+            ->add('core_explication', null, [
+            'label' => 'About core items :',
+            ])
+            ->add('optional_explication', null, [
+            'label' => 'About optional items :',
+            ])
+            ->add('conclusion', null, [
+            'label' => 'Conclusion :',
+            ]);
             
     }
 
